ebdc41fa5b110f5c89c99b3ffdf72e93dc36132a
Katia <katia.abbaoui@efrei.net>
2023-11-28 14:52:01 +0100

    delete

--- hello_word.py
+++
@@ -1,8 +0,0 @@
-print("Hello world")
-print("Hello world from Katia")
-print("Hello world from Victor")
-print("Hello world from Xiangyu")
-print("q")
-print("hello worlf ")
-
-print("Hello world from Katia again")
