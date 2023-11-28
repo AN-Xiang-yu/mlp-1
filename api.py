eb033069a9c6836866f7ac78c6fe7b7e1c823769
Xiangyu AN <xiangyu.an@efrei.net>
2023-11-28 15:08:38 +0100

    api

--- api.py
+++ api.py
@@ -1,35 +1,15 @@
 # Installed packages
-import requests
+from fastapi import FastAPI
 
-################## Constants of the Movie handler ##################
-TMDB_KEY = '6cd475d6493bd4fb6ead9f2919db145a'
-URL_MOVIE = "https://api.themoviedb.org/3/search/movie"
+# Internal packages
+from main import get_movie, tranform_movie
+
+app = FastAPI()
 
 
-def get_movie(movie_title: str, year_released: str = None) -> dict:
-    """Get the movie from the movie title.
-        Args:
-            movie_title: The title of the movie.
-            year_released: The year the movie was released.
+@app.post("/create_movie")
+def read_root():
+    movie = get_movie('The Matrix')
 
-        Returns:
-            movie: The movie from the movie title.
-    """
-    # initialisation
-    params = {"api_key": TMDB_KEY, "query": movie_title}
-
-    # make the request to the movie API
-    response = requests.get(URL_MOVIE, params=params)
-
-    # check if the request was successful
-    if response.status_code != 200 or not response.json().get('results'):
-        return None
-
-    # get the movie
-    if year_released:
-        for movie in response.json()['results']:
-            if movie['release_date'].split('-')[0] == year_released:
-                return movie
-
-    movie = response.json()['results'][0]
-    return movie
+    print(tranform_movie(movie))
+    return {"Hello": "World"}
